416580d02da8c3f733499c901da06bcd60638f72
Silver Lao <silver.lao@gmail.com>
2012-06-05 20:49:09 +0800

    bootstrap added

--- fujin8/urls.py
+++ fujin8/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'fujin8.views.home', name='home'),
+    url(r'^$', 'fujin8.views.home', name='home'),
     # url(r'^fujin8/', include('fujin8.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
